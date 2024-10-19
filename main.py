1b76caacacdb9b391db0b3d7887cef0224f54d43
Spiderest <airtempmail1@gmail.com>
2024-10-19 14:52:17 +0200

    added program

---
+++ main.py
@@ -0,0 +1,68 @@
+import asyncio, sys, os
+from bs4 import BeautifulSoup
+from telegram import Bot
+from scrapy.crawler import CrawlerProcess
+from pleazgg.spiders.spider import TokenAuthSpider
+
+#------------------------------------------------------------------------------------
+STARTLIST = '<div class="w-100 m-0 mt-2" data-chat-target="playersList">'
+ENDLIST = '<div style="display: none;" data-chat-target="playersBlocksListArea">'
+SPIDER = "scrapy crawl token_auth_spider"
+
+def startSpider():
+    process = CrawlerProcess()
+    process.crawl(TokenAuthSpider)
+    process.start()
+
+def readHtml():
+    path = os.path.join(os.getcwd(), 'source.html')
+    with open(path, 'r', encoding='utf-8') as html:
+        page = html.read()
+        page = page.split(STARTLIST)[1]
+        page = page.split(ENDLIST)[0]
+
+    soup = BeautifulSoup(page, 'html.parser')
+    div = soup.find_all('div', attrs={'data-player-nickname': True})
+    return [attribute['data-player-nickname'].lower() for attribute in div]
+
+def searchPlayers(onlinePlayers):
+    target = ['paolino885', 'jefsimons', 'ben201', 'dbn3', 'hehehe16', 'uberpapst', 
+                'asther01', 'psico', 'quartlast', 'zenomullen']
+
+    subPlayers = []
+    for nickname in onlinePlayers:
+        if nickname in target:
+            subPlayers.append(nickname)
+    
+    if subPlayers:
+        message = "Online Players:\n"
+        for i, nickname in enumerate(subPlayers):
+            message += f"{i+1}. {nickname}\n"
+    else:
+        message = "No online Players"
+        
+    return message
+
+#------------------------------------------------------------------------------------
+TOKEN = os.getenv('TELEGRAM_TOKEN')
+CHATID = os.getenv('CHAT_ID')
+
+def sendMessage(message):
+    async def send_message(message):
+        bot = Bot(TOKEN)
+        await bot.send_message(CHATID, message)
+    asyncio.run(send_message(message))
+
+if __name__ == '__main__':
+    try:
+        startSpider()
+        onlinePlayers = readHtml()
+        message = searchPlayers(onlinePlayers)
+        if message:
+            sendMessage(message)
+        else:
+            print("No message")
+    except Exception as e:
+        sendMessage(f"Failed by: {e}")
+    finally:
+        sys.exit(0)
---
+++ pleazgg/spiders/spider.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import scrapy, sys, time, gc, os
+
+class TokenAuthSpider(scrapy.Spider):
+    name = 'token_auth_spider'
+
+    def __init__(self, *args, **kwargs):
+        super(TokenAuthSpider, self).__init__(*args, **kwargs)
+        options = Options()
+        options.headless = False
+        options = Options()
+        options.add_argument("--headless")  # Aggiungi questa riga
+        options.add_argument("--no-sandbox")  # Necessario in alcuni ambienti
+        options.add_argument("--disable-dev-shm-usage")  # Necessario in alcuni ambienti
+
+        self.driver = webdriver.Chrome(service=Service(), options=options)
+
+    def start_requests(self):
+        self.driver.get("https://pleazgg.com/login")
+
+        username_input = self.driver.find_element(By.ID, "username")
+        password_input = self.driver.find_element(By.ID, "password")
+        login_button = self.driver.find_element(By.CSS_SELECTOR, "button[type='submit']")
+
+        user = os.getenv('USER')
+        password = os.getenv('PASSWORD')
+        
+        print("\n\n\n", user, password, "\n\n\n")
+        
+        username_input.send_keys(user)
+        password_input.send_keys(password)
+        login_button.click()
+        time.sleep(3)
+        
+        self.driver.get("https://pleazgg.com/lobby")
+        
+        chat_button = self.driver.find_element(By.CSS_SELECTOR, "button[data-bs-toggle='offcanvas']")
+        chat_button.click()
+        time.sleep(15)
+        
+        self.parse_chat("")
+
+    def parse_chat(self, response):
+        try:
+            path = os.path.join(os.getcwd(), 'source.html')
+            page_html = self.driver.page_source
+            with open(path, 'w', encoding='utf-8') as f:
+                f.write(page_html)
+        except Exception:
+            gc.collect()
+            self.close("")
+            sys.exit(0)
+        finally:
+            gc.collect()
+            self.close("")
+
+    def close(self, reason):
+        self.driver.quit()
